64852242f619be0f4f03a72909ff45a89bf86e53
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-13 16:20:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Simple Bot to reply to Telegram messages.
-This is built on the API wrapper, see echobot2.py to see the same example built
-on the telegram.ext bot framework.
-This program is dedicated to the public domain under the CC0 license.
-"""
-import logging
-import telegram
-from telegram.error import NetworkError, Unauthorized
-from time import sleep
+#"""Simple Bot to reply to Telegram messages.
+#This is built on the API wrapper, see echobot2.py to see the same example built
+#on the telegram.ext bot framework.
+#This program is dedicated to the public domain under the CC0 license.
+#"""
+#import logging
+#import telegram
+#from telegram.error import NetworkError, Unauthorized
+#from time import sleep
 
 
-update_id = None
+#update_id = None
 
 
-def main():
-    """Run the bot."""
-    global update_id
+#def main():
+#    """Run the bot."""
+#    global update_id
     # Telegram Bot Authorization Token
-    bot = telegram.Bot('602671114:AAFO7AajY4QtufjBgYG7y8dQb4IcpeLgWss')
+#    bot = telegram.Bot('602671114:AAFO7AajY4QtufjBgYG7y8dQb4IcpeLgWss')
 
     # get the first pending update_id, this is so we can skip over it in case
     # we get an "Unauthorized" exception.
-    try:
-        update_id = bot.get_updates()[0].update_id
-    except IndexError:
-        update_id = None
+#   try:
+#        update_id = bot.get_updates()[0].update_id
+#    except IndexError:
+#        update_id = None
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    while True:
-        try:
-            echo(bot)
-        except NetworkError:
-            sleep(1)
-        except Unauthorized:
+#    while True:
+#        try:
+#            echo(bot)
+#        except NetworkError:
+#            sleep(1)
+#        except Unauthorized:
             # The user has removed or blocked the bot.
-            update_id += 1
+#            update_id += 1
 
 
-def echo(bot):
-    """Echo the message the user sent."""
-    global update_id
+#def echo(bot):
+#    """Echo the message the user sent."""
+#    global update_id
     # Request updates after the last update_id
-    for update in bot.get_updates(offset=update_id, timeout=10):
-        update_id = update.update_id + 1
+#    for update in bot.get_updates(offset=update_id, timeout=10):
+#        update_id = update.update_id + 1
 
-        if update.message:  # your bot can receive updates without messages
+#        if update.message:  # your bot can receive updates without messages
             # Reply to the message
-            update.message.reply_text(update.message.text)
+#           update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
