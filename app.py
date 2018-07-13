ec8005ea6518c3f13530ae568a28873538ad2d8c
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-13 16:24:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 # -0,0 +1,30 
-import os
-from flask import Flask, request
-import telepot
+#import os
+#from flask import Flask, request
+#import telepot
 
-try:
-    from Queue import Queue
-except ImportError:
-    from queue import Queue
+#try:
+#    from Queue import Queue
+#except ImportError:
+#    from queue import Queue
 
-app = Flask(__name__)
-TOKEN = os.environ['602671114:AAFO7AajY4QtufjBgYG7y8dQb4IcpeLgWss']  # put your token in heroku app as environment variable
-SECRET = '/bot' + TOKEN
-URL = 'https://hackernoonbot.herokuapp.com/' #  paste the url of your application
+#app = Flask(__name__)
+#TOKEN = os.environ['602671114:AAFO7AajY4QtufjBgYG7y8dQb4IcpeLgWss']  # put your token in heroku app as environment variable
+#SECRET = '/bot' + TOKEN
+#URL = 'https://hackernoonbot.herokuapp.com/' #  paste the url of your application
 
-UPDATE_QUEUE = Queue()
-BOT = telepot.Bot(TOKEN)
+#UPDATE_QUEUE = Queue()
+#BOT = telepot.Bot(TOKEN)
 
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    BOT.sendMessage(chat_id, 'hello!')
+#def on_chat_message(msg):
+#    content_type, chat_type, chat_id = telepot.glance(msg)
+#    BOT.sendMessage(chat_id, 'hello!')
 
-BOT.message_loop({'chat': on_chat_message}, source=UPDATE_QUEUE)  # take updates from queue
+#BOT.message_loop({'chat': on_chat_message}, source=UPDATE_QUEUE)  # take updates from queue
 
-@app.route(SECRET, methods=['GET', 'POST'])
-def pass_update():
-    UPDATE_QUEUE.put(request.data)  # pass update to bot
-    return 'OK'
+#@app.route(SECRET, methods=['GET', 'POST'])
+#def pass_update():
+#    UPDATE_QUEUE.put(request.data)  # pass update to bot
+#    return 'OK'
 
-BOT.setWebhook() 
+#BOT.setWebhook() 
